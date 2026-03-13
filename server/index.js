1c436f115f87a3db5880e2e0f4d0f6ed3b700f8a
Adam Nazri <82788264+AmirulAdam1@users.noreply.github.com>
2026-03-13 14:34:17 +0800

    Update listen call

--- server/index.js
+++ server/index.js
@@ -24,7 +24,9 @@ mongoose
   .then(() => {
     console.log('Connected to MongoDB');
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server running on port ${PORT}`);
+  });
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err);
